cb7c7c86133f89a306ac0025f3631bfc34121231
YINNSTORE <146443900+YINNSTORE@users.noreply.github.com>
2024-02-29 10:09:58 +0700

    Update menu-ssh.sh

--- menu/menu-ssh.sh
+++ menu/menu-ssh.sh
@@ -1,45 +1,103 @@
 #!/bin/bash
-# MYIP=$(wget -qO- ipinfo.io/ip);
-# // color
-GREENBO="\e[92;1m"
-NC='\e[0m' # penutup
-BGGREEN="\033[42;37m" # hijau
-BGRED="\033[41;37m" # merah
-BGYELLOW="\033[43;37m" # kuning
-BGPURPLE="\033[45;37m" # ungu
-PR="\033[1;35m"
-grenbo="\e[92;1m"
-NC='\033[0m'
+RED="\033[31m"
+g="\033[1;92m"
+y='\033[1;93m'
+u="\033[0;36m"
+NC="\e[0m"
+IPVPES="https://raw.githubusercontent.com/YINNSTORE/izinsc/main/ip"
 clear
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "\033[1;93m│$NC\033[42m          MENU MANAGER SSH                $NC"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "\033[1;93m│  ${grenbo}1.${NC} \033[0;36mCreate SSH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}2.${NC} \033[0;36mTrial SSH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}3.${NC} \033[0;36mRenew SSH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}4.${NC} \033[0;36mDelet SHH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}5.${NC} \033[0;36mUser Login SSH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}6.${NC} \033[0;36mMember SSH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}7.${NC} \033[0;36mDell User SSH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}8.${NC} \033[0;36mAuto Kill SSH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}9.${NC} \033[0;36mMultiLogin SHH & OPENVPN${NC}"
-echo -e "\033[1;93m│  ${grenbo}0.${NC} \033[0;36mBack To Menu${NC}"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+ipsaya=$(curl -sS ipv4.icanhazip.com)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+checking_sc() {
+  useexp=$(wget -qO- $IPVPES | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/zheevpn"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
+#Domain
+domain=$(cat /etc/xray/domain)
+
+echo -e "${y}┌──────────────────────────────────────────┐\033[0m"
+echo -e "${y}│$NC              $g SSH & OpenVPN           ${NC}"
+echo -e "${y}└──────────────────────────────────────────┘\033[0m"
+echo -e "${y}┌──────────────────────────────────────────┐\033[0m"
+echo -e "${y}│${NC}${g} 01.${NC} $u Create SSH & OpenVPN Account${NC}"
+echo -e "${y}│${NC}${g} 02.${NC} $u Trial SSH & OpenVPN ${NC}"
+echo -e "${y}│${NC}${g} 03.${NC} $u Renew SSH & OpenVPN ${NC}"
+echo -e "${y}│${NC}${g} 04.${NC} $u Check User Login SSH & OpenVPN${NC}"
+echo -e "${y}│${NC}${g} 05.${NC} $u Daftar Member SSH & OpenVPN ${NC}"
+echo -e "${y}│${NC}${g} 06.${NC} $u Hapus SSH & OpenVpn Account ${NC}"
+echo -e "${y}│${NC}${g} 07.${NC} $u Hapus User Expired SSH & OpenVPN ${NC}"
+echo -e "${y}│${NC}${g} 08.${NC} $u Set up Autokill SSH ${NC}"
+echo -e "${y}│${NC}${g} 09.${NC} $u Cek User Multi Login SSH ${NC}"
+echo -e "${y}│${NC}${g} 10.${NC} $u Lock User SSH Account ${NC}"
+echo -e "${y}│${NC}${g} 11.${NC} $u Unlock User SSH Account ${NC}"
+echo -e "${y}│${NC}${g} 12.${NC} $u Config SSH Account ${NC}"
+echo -e "${y}│${NC}${g} 13.${NC} $u Edit Limit IP User SSH Account ${NC}"
+echo -e "${y}│${NC}${g} 00.${NC} $u Exit SSH & OpenVPN Menu${NC}"
+echo -e "${y}└──────────────────────────────────────────┘\033[0m"
+echo ""
+read -p "Select From Options [ 1 - 13 or 00 ] : " menu
 echo -e ""
-read -p " Select menu :  "  opt
-echo -e ""
-case $opt in
-1) clear ; usernew ; exit ;;
-2) clear ; trial ; exit ;;
-3) clear ; renew ; exit ;;
-4) clear ; hapus ; exit ;;
-5) clear ; cek ; exit ;;
-6) clear ; member ; exit ;;
-7) clear ; delete ; exit ;;
-8) clear ; autokill ; exit ;;
-9) clear ; ceklim ; exit ;;
-0) clear ; menu ; exit ;;
-x) exit ;;
-*) echo "Anda salah tekan " ; sleep 1 ; menu-ssh ;;
+case $menu in
+00|0)
+    menu
+    ;;
+10|1)
+    addssh
+    ;;
+02|2)
+    trial
+    ;;
+03|3)
+    renewssh
+    ;;
+04|4)
+    cekssh
+    ;;
+05|5)
+    member
+    ;;
+06|6)
+    delssh
+    ;;
+07|7)
+    delexp
+    ;;
+08|8)
+    autokill
+    ;;
+09|9)
+    ceklim
+    ;;
+10)
+    lock
+    ;;
+11)
+   unlock
+   ;;
+12)
+   user-ssh
+   ;;
+13)
+   editsship
+   ;;
+*)
+    m-sshws
+    ;;
 esac
